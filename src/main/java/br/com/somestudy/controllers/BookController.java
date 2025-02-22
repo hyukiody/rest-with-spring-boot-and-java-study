df67bee65fbf7308218948cf4e7885afdb2a7550
hyukiody <henrique.yukio@aluno.ufrb.edu.br>
2025-02-22 18:16:19 -0300

    adding analysis

--- src/main/java/br/com/somestudy/controllers/BookController.java
+++ src/main/java/br/com/somestudy/controllers/BookController.java
@@ -29,6 +29,7 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
+
 /*
  * Controller classes are the classes where the communication methods between the internal java 
  * application run and the SQL database; intermediated by standard pre-configured web applications
@@ -44,139 +45,148 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 @Tag(name = "Book", description = "Endpoints for Managing Book")
 public class BookController {
 	// This method handles HTTP GET requests to retrieve all books.
-    // It produces responses in JSON, XML, and YML formats.
-    // The @Operation annotation provides metadata for Swagger documentation.
-    // The @ApiResponse annotations specify the possible response codes and their content. standard interface communication for the client  and api interface
-	//for after sql connection, data can be set and unset from the database, and the client can send and receive data from the database.
-	
+	// It produces responses in JSON, XML, and YML formats.
+	// The @Operation annotation provides metadata for Swagger documentation.
+	// The @ApiResponse annotations specify the possible response codes and their
+	// content. standard interface communication for the client and API interface
+	// for after sql connection, data can be set and unset from the database, and
+	// the client can send and receive data from the database.
+
 	@Autowired
 	private BookServices service;
-<<<<<<< HEAD
 	/*
-	 * Each CRUD operation has it's equivalent Mapping annotation for the binding of the 
-	 * class methods and the application HTML query requisition 
+	 * Each CRUD operation has it's equivalent Mapping annotation for the binding of
+	 * the class methods and the application HTML query requisition to configure the
+	 * API requisitions that bring the various database answer types of return
 	 */
-	@GetMapping( //to configure the API requisition to return the database answer types of return
-=======
 
-	@GetMapping(// endpoint string address, mostly for formatting specification. 
->>>>>>> f87d1111b0bf24fc81439351eba5feee66fcbc41
+	@GetMapping( // endpoint string address, mostly for formatting specification.
 			produces = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML })
-		@Operation(summary = "Finds all Book", description = "Finds all Book",
-			tags = {"Book"},
-			// basically an operation might have multiple responses, so we can specify them here.
-			responses = {
-				@ApiResponse( //also not so much different from the operation annotation, but here we can specify the response code. But its important to remember these are only standardized auto code for transaction status and not the actual response code.
-				//for each transaction there is a specified object type being sent and received;
-				//so we need a return response code for each transaction, in order to confirm the transaction status. And action being performed.
-				//might call a transaction status code.
-					description = "Success", responseCode = "200",
-					content = {
-						@Content(
-							mediaType = "application/json",
-							array = @ArraySchema(schema = @Schema(implementation = BookVO.class))
-						)
-					}), //specifying each possible response code is important for the various possible responses
-				//of the API
-				@ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
-				@ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
-				@ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
-				@ApiResponse(description = "Internal Error", responseCode = "500", content = @Content),
-			}
-			//so for each being specified, we need to specify the response code, and the content being sent and received at each method dependency
-		)
-		public ResponseEntity <PagedModel<EntityModel<BookVO>>>findAll(
-				@RequestParam(value = "page", defaultValue = "0") Integer page,
-				@RequestParam(value = "size", defaultValue = "12") Integer size,
-				@RequestParam(value = "direction", defaultValue = "asc") String direction
-		){
-			var sortDirection = "desc".equalsIgnoreCase(direction) ? Direction.DESC : Direction.ASC;
-			
-			Pageable pageable = PageRequest.of(page, size, Sort.by(sortDirection, "title"));
-			return ResponseEntity.ok(service.findAll(pageable));
-		}
-		
-		@GetMapping(value = "/{id}", //mediatype specification for the response being sent back!
-			produces = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML  })
-		@Operation(summary = "Finds a Book", description = "Finds a Book",
-			tags = {"Book"},
-			responses = {
-				@ApiResponse(description = "Success", responseCode = "200",
-					content = @Content(schema = @Schema(implementation = BookVO.class))
-				),
-				@ApiResponse(description = "No Content", responseCode = "204", content = @Content),
-				@ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
-				@ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
-				@ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
-				@ApiResponse(description = "Internal Error", responseCode = "500", content = @Content),
-			}
-			//again, this is standard type of package being return are specified along with the annotation tags and the response code.
-			//basically the response code is the transaction status code, and the content is the  array of objects being sent and received as value objects or arrays of value objects
-		)
-		public BookVO findById(@PathVariable(value = "id") Long id) {
-			return service.findById(id);
-			//the pathvariable value defines html query string, and the service method is called to return the object.
-			//the object is then sent back to the client as the value object 
-			//good to remember that the value object cant be deencripted by the client, so its safe to send back the object as a default object class; for at least a class serialization is enough to encrypt the data being passed along the network.
-			//the object is then serialized and sent back to the client as a response object.
-			//because the object is serialized, the client can only see the object as a serialized object, and not the actual object. serialization is a good way to encrypt data being passed along the network.
-		}
-		
-		@PostMapping(
-			consumes = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML  },
-			produces = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML  })
-		@Operation(summary = "Adds a new Book",
-			description = "Adds a new Book by passing in a JSON, XML or YML representation of the book!",
-			tags = {"Book"},
-			responses = {
-				@ApiResponse(description = "Success", responseCode = "200",
-					content = @Content(schema = @Schema(implementation = BookVO.class))
-				),
-				@ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
-				@ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
-				@ApiResponse(description = "Internal Error", responseCode = "500", content = @Content),
-			}
-		)
-		public BookVO create(@RequestBody BookVO book) {
-			return service.create(book);
-		}
-		
-		@PutMapping(
-			consumes = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML  },
-			produces = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML  })
-		@Operation(summary = "Updates a Book",
-			description = "Updates a Book by passing in a JSON, XML or YML representation of the book!",
-			tags = {"Book"},
-			responses = {
-				@ApiResponse(description = "Updated", responseCode = "200",
-					content = @Content(schema = @Schema(implementation = BookVO.class))
-				),
-				@ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
-				@ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
-				@ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
-				@ApiResponse(description = "Internal Error", responseCode = "500", content = @Content),
-			}
-		)
-		public BookVO update(@RequestBody BookVO book) {
-			return service.update(book);
-		}
-		
-		
-		@DeleteMapping(value = "/{id}")
-		@Operation(summary = "Deletes a Book",
-			description = "Deletes a Book by passing in a JSON, XML or YML representation of the book!",
-			tags = {"Book"},
-			responses = {
-				@ApiResponse(description = "No Content", responseCode = "204", content = @Content),
-				@ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
-				@ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
-				@ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
-				@ApiResponse(description = "Internal Error", responseCode = "500", content = @Content),
-			}
-		)
-		public ResponseEntity<?> delete(@PathVariable(value = "id") Long id) {
-			service.delete(id);
-			return ResponseEntity.noContent().build();
-		}
+	@Operation(summary = "Finds all Book", description = "Finds all Book", tags = { "Book" },
+			// basically an operation might have multiple responses, so we can specify them
+			// here.
+			responses = { @ApiResponse( 
+					
+					/*
+					 * also not so much different from the operation annotation, but here we can
+					 * specify the response codes. Its important to remember these are only standard
+					 * auto code for transaction status and not the actual data package being
+					 * returned. for each transaction there is a specified object type being sent
+					 * and received; so we need a return response code for each transaction, in
+					 * order to confirm the transaction status. And action being performed. might
+					 * call a transaction status code.
+					 * 
+					 */
+					description = "Success", responseCode = "200", content = {
+							@Content(mediaType = "application/json", array = @ArraySchema(schema = @Schema(implementation = BookVO.class))) }),
+					/*
+					 * specifying each possible response code is important for the various possible
+					 * responses of the API
+					 */
+					@ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+					@ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+					@ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+					@ApiResponse(description = "Internal Error", responseCode = "500", content = @Content), }
 	
+	/*
+	 * For each being API Response specified, we need to specify the response code, and the
+	 * content being sent and received at each method dependency
+	 */
+	)
+	
+	public ResponseEntity<PagedModel<EntityModel<BookVO>>> findAll(
+			@RequestParam(value = "page", defaultValue = "0") Integer page,
+			@RequestParam(value = "size", defaultValue = "12") Integer size,
+			@RequestParam(value = "direction", defaultValue = "asc") String direction) {
+		/*
+		 * This method finishes the interaction of the API with the service configuration for the original object VO,
+		 * beginning as a whole new method, but eventually calling the correspondent Service Method findALL() like the
+		 * other methods of the Controller class; this is necessary because the API needs another type of 
+		 * return class, that must be set through the procedure below of retrieving parameters from the API;
+		 * since the application requests are made by calling the controller methods.  
+		 * 
+		 * When the client sends a GET request to the endpoint, the local instance intercepts it
+		 */
+		var sortDirection = "desc".equalsIgnoreCase(direction) ? Direction.DESC : Direction.ASC;
+		//then Pageable object is created with parameters
+		Pageable pageable = PageRequest.of(page, size, Sort.by(sortDirection, "title"));
+		return ResponseEntity.ok(service.findAll(pageable));
+	}
+
+	@GetMapping(value = "/{id}",
+			// media type specification for the response being sent back!
+			produces = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML })
+	@Operation(summary = "Finds a Book", description = "Finds a Book", tags = { "Book" }, responses = {
+			@ApiResponse(description = "Success", responseCode = "200", content = @Content(schema = @Schema(implementation = BookVO.class))),
+			@ApiResponse(description = "No Content", responseCode = "204", content = @Content),
+			@ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+			@ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+			@ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+			@ApiResponse(description = "Internal Error", responseCode = "500", content = @Content), }
+	/*
+	 * Again, this is standard setup for package transaction status code specified along with
+	 * the annotation tags and the response code. Basically the response code is the 
+	 * transaction status response, and the content might be array of objects being sent 
+	 * or received as value objects (DTOs, since we have the primary model objects being 
+	 * occluded for initial security measures in the database requisitions)
+	
+	 */
+	)
+	public BookVO findById(@PathVariable(value = "id") Long id) {
+		return service.findById(id);
+		
+		/*
+		 * The path variable value defines HTML query string, and the service method is
+		 * called to return the object. The object is then sent back to the client as 
+		 * the value object. Good to remember that the value object can't be deencripted 
+		 * by the client, so its safe to send back the object as a default object class;
+		 * for at least a class serialization is enough to encrypt the data being passed 
+		 * along the network. The object is then serialized and sent back to the client 
+		 * as the standard response object.
+		 * Because the object is serialized, the client can only see the object as a
+		 * serialized object, and not the original object model. Serialization is a good way to
+		 * encrypt data being passed along the network.
+		
+		 */
+	}
+
+	@PostMapping(consumes = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML,
+			MediaType.APPLICATION_YML }, produces = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML,
+					MediaType.APPLICATION_YML })
+	@Operation(summary = "Adds a new Book", description = "Adds a new Book by passing in a JSON, XML or YML representation of the book!", tags = {
+			"Book" }, responses = {
+					@ApiResponse(description = "Success", responseCode = "200", content = @Content(schema = @Schema(implementation = BookVO.class))),
+					@ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+					@ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+					@ApiResponse(description = "Internal Error", responseCode = "500", content = @Content), })
+	public BookVO create(@RequestBody BookVO book) {
+		return service.create(book);
+	}
+
+	@PutMapping(consumes = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML,
+			MediaType.APPLICATION_YML }, produces = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML,
+					MediaType.APPLICATION_YML })
+	@Operation(summary = "Updates a Book", description = "Updates a Book by passing in a JSON, XML or YML representation of the book!", tags = {
+			"Book" }, responses = {
+					@ApiResponse(description = "Updated", responseCode = "200", content = @Content(schema = @Schema(implementation = BookVO.class))),
+					@ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+					@ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+					@ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+					@ApiResponse(description = "Internal Error", responseCode = "500", content = @Content), })
+	public BookVO update(@RequestBody BookVO book) {
+		return service.update(book);
+	}
+
+	@DeleteMapping(value = "/{id}")
+	@Operation(summary = "Deletes a Book", description = "Deletes a Book by passing in a JSON, XML or YML representation of the book!", tags = {
+			"Book" }, responses = { @ApiResponse(description = "No Content", responseCode = "204", content = @Content),
+					@ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+					@ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+					@ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+					@ApiResponse(description = "Internal Error", responseCode = "500", content = @Content), })
+	public ResponseEntity<?> delete(@PathVariable(value = "id") Long id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+
 }
--- src/main/java/br/com/somestudy/services/BookServices.java
+++ src/main/java/br/com/somestudy/services/BookServices.java
@@ -3,6 +3,7 @@ package br.com.somestudy.services;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
+import java.util.List;
 import java.util.logging.Logger;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,15 +21,30 @@ import br.com.somestudy.exceptions.ResourceNotFoundException;
 import br.com.somestudy.mapper.DozerMapper;
 import br.com.somestudy.model.Book;
 import br.com.somestudy.repositories.BookRepository;
-
-
-//Logger is a class in java.util.logging package. It is used to log the information in the log file. The Logger object is used to log messages for a specific system or application component. 
-//Apparently, the logger is used to log the messages to the console, file, network, etc.
-
-//since each action operated is a transaction between data objects and response codes, the logger is used to log the transaction status and the state of the action being performed at the end.
-
-//SERVICE CLASS brings within its methods implementation integration with repository, usage of Model Mapper static association return, and logger.
-
+/*
+ *  
+ * Logger class is used to log the messages to the main API prompt.
+ * 
+ * Since each action made in the API is essentially the application's translation
+ * of the HTML query and the SQL database requisitions responses to the 
+ * ongoing local java program, the service class is important for local java methods 
+ * nesting, clean code architecture and that makes possible the direct call into the
+ * CONTROLLER classes, that are the nesting of the SPRING API annotations and the local
+ * Java program.
+ * 
+ * SERVICE CLASSES brings within its methods implementation integrates repository,
+ * and the usage of the chosen ModelMapper dependencies for class type conversions
+ * between the DTO API interface treatment class and the local running logical model
+ * classes integrates the object occlusion between the user interface and the local 
+ * program execution instance.
+ * 
+ * Here is a brief description of the PageModel<EntityModel<BookVO>> class method used here in the service
+ * class for posterior referencing in the controller class:
+ * 		The EntityModel is a Spring HATEOAS class; its called a wrapper class made for
+ * 		attaching hypermedia links to the object, but it obligates the implementation
+ * 		of the first object model to this 
+ *  
+*/
 @Service
 public class BookServices {
 
@@ -36,80 +52,83 @@ public class BookServices {
 
 	@Autowired
 	BookRepository repository;
-<<<<<<< HEAD
-	
+
 	@Autowired
 	PagedResourcesAssembler<BookVO> assembler;
+
+	/*
+	 * Credits for the project author : the usage of dependencies 
+	 * that make possible for the page modeling mechanism seems is advanced. 
+	 * The method declaration initiates as a cast call;
+	 */
 	
 	public PagedModel <EntityModel<BookVO>> findAll(Pageable pageable){
 		
-=======
-
-	
-	//The ModelMapper class provides a mapping between two objects. It is a part of the ModelMapper module.
-	//
-	public List<BookVO> findAll() {
->>>>>>> f87d1111b0bf24fc81439351eba5feee66fcbc41
+		
 		logger.info("Finding all books!");
-		
+
 		var booksPage = repository.findAll(pageable);
-		
-		var booksVOs= booksPage.map(p -> DozerMapper.parseObject(p, BookVO.class));
-		
+		//class conversion through the current optimized model mapper native method
+		var booksVOs = booksPage.map(p -> DozerMapper.parseObject(p, BookVO.class));
+		//
 		booksVOs.map(p -> p.add(linkTo(methodOn(BookController.class).findById(p.getKey())).withSelfRel()));
-		
-		Link findAllLink = linkTo(methodOn(BookController.class).findAll(pageable.getPageNumber(),pageable.getPageSize(),"asc")).withSelfRel();
-		
+
+		Link findAllLink = linkTo(
+				methodOn(BookController.class).findAll(pageable.getPageNumber(), pageable.getPageSize(), "asc"))
+				.withSelfRel();
+
 		return assembler.toModel(booksVOs, findAllLink);
 	}
-	
-
 
 	public BookVO findById(Long id) {
 		logger.info("Finding one book!");
-		
-		var entity = repository.findById(id).orElseThrow(() -> new ResourceNotFoundException("No records found for this ID!"));
+
+		var entity = repository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("No records found for this ID!"));
 		var vo = DozerMapper.parseObject(entity, BookVO.class);
 		vo.add(linkTo(methodOn(BookController.class).findById(id)).withSelfRel());
 		return vo;
 	}
-	
+
 	public BookVO create(BookVO book) {
-		if(book == null) throw new RequiredObjectIsNullException();
-		
+		if (book == null)
+			throw new RequiredObjectIsNullException();
+
 		logger.info("Creating one book!");
-		
+
 		var entity = DozerMapper.parseObject(book, Book.class);
 		var vo = DozerMapper.parseObject(repository.save(entity), BookVO.class);
 		vo.add(linkTo(methodOn(BookController.class).findById(vo.getKey())).withSelfRel());
 		return vo;
 	}
-	
+
 	public BookVO update(BookVO book) {
-		if(book == null) throw new RequiredObjectIsNullException();
-		
+		if (book == null)
+			throw new RequiredObjectIsNullException();
+
 		logger.info("Updating one book!");
-		
-		var entity= repository.findById(book.getKey()).orElseThrow(() -> new ResourceNotFoundException("No records found for this ID!"));
+
+		var entity = repository.findById(book.getKey())
+				.orElseThrow(() -> new ResourceNotFoundException("No records found for this ID!"));
 		entity.setAuthor(book.getAuthor());
 		entity.setLaunchDate(book.getLaunchDate());
 		entity.setPrice(book.getPrice());
 		entity.setTitle(book.getTitle());
-		
+
 		var vo = DozerMapper.parseObject(repository.save(entity), BookVO.class);
 		vo.add(linkTo(methodOn(BookController.class).findById(vo.getKey())).withSelfRel());
 		return vo;
 	}
+
 	public void delete(Long id) {
 		logger.info("Deleting one book!");
-		
+
 		var entity = repository.findById(id)
 				.orElseThrow(() -> new ResourceNotFoundException("No records found for this ID!"));
-		
+
 		repository.delete(entity);
 	}
 
-	
 }
 /*
  * 
