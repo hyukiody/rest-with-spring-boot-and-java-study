140a36337c1cbc7d5b0e2f0e9a3acb09ffa2aa42
hyukiody <henrique.yukio@aluno.ufrb.edu.br>
2025-03-16 16:24:46 -0300

    authentication working

--- src/main/java/br/com/somestudy/repositories/UserRepository.java
+++ src/main/java/br/com/somestudy/repositories/UserRepository.java
@@ -9,7 +9,8 @@ import br.com.somestudy.model.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long>{
-	@Query("SELECT u FROM User u WHERE u.userName =: userName")
+	
+	@Query("SELECT u FROM User u WHERE u.userName = :userName")
 	User findByUserName(@Param("userName") String userName);
 
 }
